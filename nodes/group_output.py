d454c2e0459f8f4df76acc4ae28c6b7d418e035e
pvtrcorps <antoniojaviertorralbamoreno@gmail.com>
2025-06-22 21:16:43 +0100

    Sync group nodes with tree interface

--- nodes/group_output.py
+++ nodes/group_output.py
@@ -2,7 +2,13 @@
 import bpy
 from bpy.types import Node
 from .base import FNBaseNode
-from ..sockets import FNSocketScene
+
+
+def _interface_outputs(tree):
+    iface = getattr(tree, "interface", None)
+    for item in getattr(iface, "items_tree", []):
+        if getattr(item, "in_out", None) == 'OUTPUT':
+            yield item
 
 class FNGroupOutputNode(Node, FNBaseNode):
     bl_idname = "FNGroupOutputNode"
@@ -13,7 +19,28 @@ class FNGroupOutputNode(Node, FNBaseNode):
         return ntree.bl_idname == "FileNodesTreeType"
 
     def init(self, context):
-        self.inputs.new('FNSocketScene', "Scene")
+        self._sync_with_interface()
+
+    def update(self):
+        self._sync_with_interface()
+
+    def _sync_with_interface(self):
+        tree = self.id_data
+        if not tree:
+            return
+        iface_sockets = list(_interface_outputs(tree))
+        names = [s.name for s in iface_sockets]
+        for sock in list(self.inputs):
+            if sock.name not in names:
+                self.inputs.remove(sock)
+        for item in iface_sockets:
+            sock = self.inputs.get(item.name)
+            if sock is None:
+                sock = self.inputs.new(item.socket_type, item.name)
+            elif sock.bl_idname != item.socket_type:
+                self.inputs.remove(sock)
+                sock = self.inputs.new(item.socket_type, item.name)
+            sock.name = item.name
 
     def process(self, context, inputs):
         # Terminal node, no action needed
