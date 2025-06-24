eee12b046fe7f6ff9db568ad1507d5c68f739925
pvtrcorps <antoniojaviertorralbamoreno@gmail.com>
2025-06-24 06:44:54 +0100

    Cambiar entradas de nombre a sockets

--- nodes/new_collection.py
+++ nodes/new_collection.py
@@ -1,45 +1,24 @@
 import bpy
 from bpy.types import Node
 from .base import FNBaseNode
-from ..sockets import FNSocketCollection
-from ..operators import auto_evaluate_if_enabled
+from ..sockets import FNSocketCollection, FNSocketString
 
 class FNNewCollection(Node, FNBaseNode):
     bl_idname = "FNNewCollection"
     bl_label = "New Collection"
-
-    name: bpy.props.StringProperty(name="Name", default="Collection", update=auto_evaluate_if_enabled)
-    color_tag: bpy.props.EnumProperty(
-        name="Color Tag",
-        items=[
-            ('NONE', 'None', ''),
-            ('COLOR_01', '01', ''),
-            ('COLOR_02', '02', ''),
-            ('COLOR_03', '03', ''),
-            ('COLOR_04', '04', ''),
-            ('COLOR_05', '05', ''),
-            ('COLOR_06', '06', ''),
-            ('COLOR_07', '07', ''),
-            ('COLOR_08', '08', ''),
-        ],
-        default='NONE',
-        update=auto_evaluate_if_enabled
-    )
 
     @classmethod
     def poll(cls, ntree):
         return ntree.bl_idname == "FileNodesTreeType"
 
     def init(self, context):
+        sock = self.inputs.new('FNSocketString', "Name")
+        sock.value = "Collection"
         self.outputs.new('FNSocketCollection', "Collection")
 
-    def draw_buttons(self, context, layout):
-        layout.prop(self, "name", text="Name")
-        layout.prop(self, "color_tag", text="Color Tag")
-
     def process(self, context, inputs):
-        coll = bpy.data.collections.new(self.name)
-        coll.color_tag = self.color_tag
+        name = inputs.get("Name") or "Collection"
+        coll = bpy.data.collections.new(name)
         return {"Collection": coll}
 
 
