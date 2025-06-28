ccfaa28c0c6d64e61e2f26a030674b22444b5bd2
pvtrcorps <antoniojaviertorralbamoreno@gmail.com>
2025-06-28 16:19:45 +0100

    Fix API compatibility

--- tree.py
+++ tree.py
@@ -204,7 +204,7 @@ class FileNodesTree(NodeTree):
     fn_enabled: bpy.props.BoolProperty(name='Enabled', default=True)
     fn_inputs: bpy.props.PointerProperty(type=FileNodesTreeInputs)
 
-    def interface_update(self, context=None):
+    def interface_update(self, context):
         if getattr(self, "fn_inputs", None):
             self.fn_inputs.sync_inputs(self)
 
@@ -250,7 +250,7 @@ class FileNodesTree(NodeTree):
         return walk(self)
 
     def update(self):
-        self.interface_update()
+        self.interface_update(bpy.context)
         if getattr(self, "fn_inputs", None):
             self.fn_inputs.restore_and_clear()
 
