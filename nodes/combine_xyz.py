a06071c8b784f32b7247c64fe6d69d908d87fe99
pvtrcorps <antoniojaviertorralbamoreno@gmail.com>
2025-06-28 11:52:30 +0100

    Add Combine/Separate XYZ nodes

---
+++ nodes/combine_xyz.py
@@ -0,0 +1,30 @@
+import bpy
+from bpy.types import Node
+
+from .base import FNBaseNode
+from ..sockets import FNSocketFloat, FNSocketVector
+
+
+class FNCombineXYZ(Node, FNBaseNode):
+    bl_idname = "FNCombineXYZ"
+    bl_label = "Combine XYZ"
+
+    def init(self, context):
+        self.inputs.new('FNSocketFloat', "X")
+        self.inputs.new('FNSocketFloat', "Y")
+        self.inputs.new('FNSocketFloat', "Z")
+        self.outputs.new('FNSocketVector', "Vector")
+
+    def process(self, context, inputs):
+        x = inputs.get("X") or 0.0
+        y = inputs.get("Y") or 0.0
+        z = inputs.get("Z") or 0.0
+        return {"Vector": (x, y, z)}
+
+
+def register():
+    bpy.utils.register_class(FNCombineXYZ)
+
+
+def unregister():
+    bpy.utils.unregister_class(FNCombineXYZ)
---
+++ nodes/separate_xyz.py
@@ -0,0 +1,32 @@
+import bpy
+from bpy.types import Node
+
+from .base import FNBaseNode
+from ..sockets import FNSocketFloat, FNSocketVector
+
+
+class FNSeparateXYZ(Node, FNBaseNode):
+    bl_idname = "FNSeparateXYZ"
+    bl_label = "Separate XYZ"
+
+    def init(self, context):
+        self.inputs.new('FNSocketVector', "Vector")
+        self.outputs.new('FNSocketFloat', "X")
+        self.outputs.new('FNSocketFloat', "Y")
+        self.outputs.new('FNSocketFloat', "Z")
+
+    def process(self, context, inputs):
+        vec = inputs.get("Vector")
+        if isinstance(vec, (list, tuple)) and len(vec) >= 3:
+            x, y, z = vec[:3]
+        else:
+            x = y = z = 0.0
+        return {"X": x, "Y": y, "Z": z}
+
+
+def register():
+    bpy.utils.register_class(FNSeparateXYZ)
+
+
+def unregister():
+    bpy.utils.unregister_class(FNSeparateXYZ)
