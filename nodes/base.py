884e551068d01780247d01c2fb74089f47bef166
pvtrcorps <antoniojaviertorralbamoreno@gmail.com>
2025-06-28 15:23:29 +0200

    Revert "Merge pull request #116 from pvtrcorps/codex/cambiar-color-del-header-de-nodos-a-rgb-0.2-0.2-0.2"
    This reverts commit 167298b3c15e8d20dfeee2e3c12be4c094b64617, reversing
    changes made to dd734ae96bb3db78d95ece3d9cebcb8ee82cb5ec.

--- nodes/base.py
+++ nodes/base.py
@@ -22,28 +22,8 @@ class FNCacheIDMixin:
         self._cached_id = None
 
 class FNBaseNode:
-    '''Mixin to add a .process(context, inputs) stub and default node color.'''
-
-    DEFAULT_COLOR = (0.2, 0.2, 0.2)
-
+    '''Mixin to add a .process(context, inputs) stub'''
     bl_width_default = 160
-
-    def __init_subclass__(cls) -> None:  # pragma: no cover - executed at import
-        """Ensure subclasses apply the default header color."""
-        super().__init_subclass__()
-        original_init = getattr(cls, "init", None)
-
-        def _init(self, context):
-            if original_init:
-                original_init(self, context)
-            try:
-                self.use_custom_color = True
-                self.color = cls.DEFAULT_COLOR
-            except Exception:
-                pass
-
-        cls.init = _init
-
     def process(self, context, inputs):
         return {}
 
