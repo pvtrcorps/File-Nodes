3417218f8d7475bc281370b3f603363423b80d25
pvtrcorps <antoniojaviertorralbamoreno@gmail.com>
2025-06-28 16:28:10 +0200

    Revert "Merge pull request #117 from pvtrcorps/iz3skr-codex/cambiar-color-del-header-de-nodos-a-rgb-0.2-0.2-0.2"
    This reverts commit be638031c58f9fbbac7ce8875753e6556ce2e0bc, reversing
    changes made to 884e551068d01780247d01c2fb74089f47bef166.

--- nodes/base.py
+++ nodes/base.py
@@ -22,11 +22,8 @@ class FNCacheIDMixin:
         self._cached_id = None
 
 class FNBaseNode:
-    '''Mixin to add a .process(context, inputs) stub and default header color.'''
+    '''Mixin to add a .process(context, inputs) stub'''
     bl_width_default = 160
-    # Default header color for all nodes
-    bl_color = (0.2, 0.2, 0.2)
-
     def process(self, context, inputs):
         return {}
 
